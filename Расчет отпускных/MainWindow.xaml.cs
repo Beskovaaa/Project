49ccdce575109f66cf46649e4e2e25ce0a5445c7
Beskovaaa <kuzmina.e.a.9@gmail.com>
2021-11-06 16:59:20 +0300

    Добавлен Spread на форму

--- Расчет отпускных/MainWindow.xaml.cs
+++ Расчет отпускных/MainWindow.xaml.cs
@@ -28,68 +28,68 @@ namespace Расчет_отпускных
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //Create an instance of ExcelEngine
-            using (ExcelEngine excelEngine = new ExcelEngine())
-            {
-                IApplication application = excelEngine.Excel;
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //Create an instance of ExcelEngine
+        //    using (ExcelEngine excelEngine = new ExcelEngine())
+        //    {
+        //        IApplication application = excelEngine.Excel;
 
-                application.DefaultVersion = ExcelVersion.Excel2016;
+        //        application.DefaultVersion = ExcelVersion.Excel2016;
 
-                //Create a workbook
-                IWorkbook workbook = application.Workbooks.Create(1);
-                IWorksheet worksheet = workbook.Worksheets[0];
+        //        //Create a workbook
+        //        IWorkbook workbook = application.Workbooks.Create(1);
+        //        IWorksheet worksheet = workbook.Worksheets[0];
 
-                //Adding a picture
-                FileStream imageStream = new FileStream("../../Image/DQS-Logo.jpg", FileMode.Open, FileAccess.Read);
-                IPictureShape shape = worksheet.Pictures.AddPicture(1, 1, imageStream,15,10);
-                worksheet.Range["A1:A2"].Merge();
-                worksheet.Range["A1:C1"].Merge();
+        //        //Adding a picture
+        //        FileStream imageStream = new FileStream("../../Image/DQS-Logo.jpg", FileMode.Open, FileAccess.Read);
+        //        IPictureShape shape = worksheet.Pictures.AddPicture(1, 1, imageStream,15,10);
+        //        worksheet.Range["A1:A2"].Merge();
+        //        worksheet.Range["A1:C1"].Merge();
 
-                //Disable gridlines in the worksheet
-                worksheet.IsGridLinesVisible = false;
+        //        //Disable gridlines in the worksheet
+        //        worksheet.IsGridLinesVisible = false;
 
-                //Enter values to the cells from A3 to A5
-                worksheet.Range["A6"].Text = "150003, г.Ярославль";
-                worksheet.Range["A7"].Text = "dqs@dqs-russia.ru";
-                worksheet.Range["A8"].Text = "Phone: +7 4852 69 50 21";
+        //        //Enter values to the cells from A3 to A5
+        //        worksheet.Range["A6"].Text = "150003, г.Ярославль";
+        //        worksheet.Range["A7"].Text = "dqs@dqs-russia.ru";
+        //        worksheet.Range["A8"].Text = "Phone: +7 4852 69 50 21";
 
-                //Make the text bold
-                worksheet.Range["A6:A8"].CellStyle.Font.Bold = true;
+        //        //Make the text bold
+        //        worksheet.Range["A6:A8"].CellStyle.Font.Bold = true;
 
-                //Merge cells
-                worksheet.Range["F1:N1"].Merge();
+        //        //Merge cells
+        //        worksheet.Range["F1:N1"].Merge();
 
-                //Enter text to the cell D1 and apply formatting.
-                worksheet.Range["F1"].Text = "ОРГАН ПО СЕРТИФИКАЦИИ ООО ССУ ДЭКУЭС";
-                worksheet.Range["F1"].CellStyle.Font.Bold = true;
-                worksheet.Range["F1"].CellStyle.Font.RGBColor = System.Drawing.Color.FromArgb(42, 118, 189);
-                worksheet.Range["F1"].CellStyle.Font.Size = 18;
-
-
-                //Create table with the data in given range
-                IListObject table = worksheet.ListObjects.Create("Table", worksheet["E6:O19"]);
-
-                //Create data
-                worksheet.Range["E6"].Text = "№";                
-                worksheet.Range["F6"].Text = "Месяц";
-                worksheet.Range["G6"].Text = "Кол-во дней больничного";
-                worksheet.Range["H6"].Text = "Кол-во дней отпуска";
-                worksheet.Range["I6"].Text = "Итого дней";
-                worksheet.Range["J6"].Text = "Сумма ЗП";
-                worksheet.Range["K6"].Text = "Сумма больничных";
-                worksheet.Range["L6"].Text = "Сумма отпускных";
-                worksheet.Range["M6"].Text = "ЗП в месяц без больгичных и отпускных";
-                worksheet.Range["N6"].Text = "Дни для расчета ";
-                worksheet.Range["E6:N6"].CellStyle.Font.Bold = true;
+        //        //Enter text to the cell D1 and apply formatting.
+        //        worksheet.Range["F1"].Text = "ОРГАН ПО СЕРТИФИКАЦИИ ООО ССУ ДЭКУЭС";
+        //        worksheet.Range["F1"].CellStyle.Font.Bold = true;
+        //        worksheet.Range["F1"].CellStyle.Font.RGBColor = System.Drawing.Color.FromArgb(42, 118, 189);
+        //        worksheet.Range["F1"].CellStyle.Font.Size = 18;
 
 
-                //Save the Excel document
-                workbook.SaveAs("Расчет.xlsx");
+        //        //Create table with the data in given range
+        //        IListObject table = worksheet.ListObjects.Create("Table", worksheet["E6:N19"]);
 
-                System.Diagnostics.Process.Start("Расчет.xlsx");
-            }
-        }
+        //        //Create data
+        //        worksheet.Range["E6"].Text = "№";                
+        //        worksheet.Range["F6"].Text = "Месяц";
+        //        worksheet.Range["G6"].Text = "Кол-во дней больничного";
+        //        worksheet.Range["H6"].Text = "Кол-во дней отпуска";
+        //        worksheet.Range["I6"].Text = "Итого дней";
+        //        worksheet.Range["J6"].Text = "Сумма ЗП";
+        //        worksheet.Range["K6"].Text = "Сумма больничных";
+        //        worksheet.Range["L6"].Text = "Сумма отпускных";
+        //        worksheet.Range["M6"].Text = "ЗП в месяц без больгичных и отпускных";
+        //        worksheet.Range["N6"].Text = "Дни для расчета ";
+        //        worksheet.Range["E6:N6"].CellStyle.Font.Bold = true;
+
+
+        //        //Save the Excel document
+        //        workbook.SaveAs("Расчет.xlsx");
+
+        //        System.Diagnostics.Process.Start("Расчет.xlsx");
+        //    }
+        //}
     }
 }
